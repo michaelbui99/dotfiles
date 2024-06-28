de74d3725ac99e773e4cdec042c53ff3e0f649d6
Michael Bui <michaelbui99@hotmail.com>
2024-06-28 21:08:54 +0200

    fix symlinks

--- symlink.ps1
+++ symlink.ps1
@@ -2,7 +2,7 @@ $windowsTerminalSettingsPath = "$env:userprofile\AppData\Local\Packages\Microsof
 
 New-Item -ItemType SymbolicLink -Force -Path $windowsTerminalSettingsPath -Target "$env:userprofile\dotfiles\windows-terminal\settings.json";
 New-Item -ItemType SymbolicLink -Force -Path $PROFILE -Target $env:USERPROFILE\dotfiles\powershell\Profile.ps1;
-New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.vimrc -Target $env:USERPROFILE\vim\.vimrc
-New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.ideavimrc -Target $env:USERPROFILE\vim\.ideavimrc
-New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.config/starship.toml -Target $env:USERPROFILE\starship\starship.toml
+New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.vimrc -Target $env:USERPROFILE\dotfiles\vim\.vimrc
+New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.ideavimrc -Target $env:USERPROFILE\dotfiles\vim\.ideavimrc
+New-Item -ItemType SymbolicLink -Force -Path $env:USERPROFILE/.config/starship.toml -Target $env:USERPROFILE\dotfiles\starship\starship.toml
 
