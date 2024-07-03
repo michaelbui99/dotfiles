57750566a5b2ed1b579a1dfc934c198405414250
Michael Bui <mttb@ok.dk>
2024-07-03 13:23:10 +0200

    symlinks

--- symlink.sh
+++ symlink.sh
@@ -7,7 +7,7 @@ ln -fs ~/dotfiles/bash/.bashrc ~/.bashrc && \
     ln -fs ~/dotfiles/bash/.bash_profile ~/.bash_profile && \
     ln -fs ~/dotfiles/bash/.inputrc ~/.inputrc && \
     ln -fs ~/dotfiles/starship/starship.toml ~/.config/starship.toml && \
-    ln -fs ~/dotfiles/vim/.vimrc  ~/.vimrc
-    ln -fs ~/dotfiles/vim/.ideavimrc ~/.ideavimrc
-    ln -fs ~/dotfiles/vim/.config/nvim ~/.config/nvim
+    ln -fs ~/dotfiles/vim/.vimrc  ~/.vimrc && \ 
+    ln -fs ~/dotfiles/vim/.ideavimrc ~/.ideavimrc && \
+    ln -fs ~/dotfiles/vim/.config/nvim ~/.config/nvim && \
     ln -fs ~/dotfiles/zellij/config.kdl ~/.config/zellij/config.kdl
