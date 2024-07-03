f05c1918d0c50fdc4b7890727e72a1d7401f83e2
Michael Bui <michaelbui99@hotmail.com>
2024-07-03 19:01:47 +0200

    Update lsp configs

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -22,7 +22,7 @@ return {
    		ensure_installed = {
    			"lua-language-server", "stylua",
    			"html-lsp", "css-lsp" , "prettier",
-        "rust-analyzer", "tsserver"
+        "rust-analyzer", "typescript-language-server", "gopls"
    		},
    	},
    },
